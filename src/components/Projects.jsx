c8feb3a62d5d07ef2b52adf5b28cad6aa20990cb
skrillzofficial <obazeefelixadetayo@gmail.com>
2025-09-21 17:37:17 +0100

    Feat: Eventra

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,7 +2,8 @@ import React from "react";
 import { ExternalLink, Github } from "lucide-react";
 import linkshort from "../assets/Link shortner.png";
 import sentient from "../assets/sentient.png";
-import BetaHouse from "../assets/Beta H.png"
+import BetaHouse from "../assets/Beta H.png";
+import Eventra from "../assets/Eventra.png"
 const Projects = () => {
   return (
     <div>
@@ -236,10 +237,14 @@ const Projects = () => {
               {/* Content container*/}
               <div className="w-full md:w-1/2 mt-10">
                 <h3 className="text-2xl font md:text-3xl font-bold text-white mb-4">
-                AI platform for agent battles.
+                  AI platform for agent battles.
                 </h3>
                 <p className="font mb-6 leading-relaxed">
-                  Co-developed Sentient struggle, a platform where AI meets survival. Collaborated on enabling users to create, and watch AI agents battle experiences. Key contributions included developing the real-time spectator system and implementing the payment infrastructure.
+                  Co-developed Sentient struggle, a platform where AI meets
+                  survival. Collaborated on enabling users to create, and watch
+                  AI agents battle experiences. Key contributions included
+                  developing the real-time spectator system and implementing the
+                  payment infrastructure.
                 </p>
 
                 <div className="mb-4">
@@ -273,6 +278,76 @@ const Projects = () => {
                   >
                     <span>VIEW PROJECT</span>
                     <ExternalLink size={16} />
+                  </a>
+                </div>
+              </div>
+            </div>
+            {/* Project 5 */}
+            <div className="flex flex-col md:flex-row gap-10 items-start mb-32">
+              {/* Image container */}
+              <div className="w-full md:w-1/2 relative group">
+                <div className="bg-gray-300/10 rounded-lg p-6 flex flex-col h-full">
+                  <div className="md:mb-5 bg-black w-40 text-center rounded-full mb-4">
+                    <h3 className="text-lg font">Solo-Build work</h3>
+                  </div>
+                  <div className="flex justify-center items-center p-4 h-80">
+                    <img
+                      src={Eventra}
+                      alt="Landing page for shorter links"
+                      className="max-h-full max-w-full object-contain group-hover:scale-105 transition-transform duration-700"
+                    />
+                  </div>
+                </div>
+              </div>
+
+              {/* Content container*/}
+              <div className="w-full md:w-1/2 mt-10">
+                <h3 className="text-2xl font md:text-3xl font-bold text-white mb-4">
+                  Your event, our entry
+                </h3>
+                <p className="font mb-6 leading-relaxed">
+                  Eventry is the all-in-one event management platform that
+                  empowers you to seamlessly plan, organize, and execute any
+                  event, from large corporate conferences to deeply personal
+                  wedding celebrations. We provide the tools; you create the
+                  magic.
+                </p>
+
+                <div className="mb-4">
+                  <p className="text-white">PROJECT INFO</p>
+                </div>
+
+                <div className="flex flex-col gap-2 border-t border-gray-800 pt-2">
+                  <div className="flex font justify-between">
+                    <span>Year</span>
+                    <span>2025</span>
+                  </div>
+                  <div className="border-t font border-gray-800 pt-2">
+                    <div className="flex justify-between">
+                      <span>Role</span>
+                      <span>Full-stack developer</span>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="flex gap-4 mb-6 pt-4 border-t border-gray-800">
+                  <a
+                    href="https://eventra-yyo5.vercel.app//"
+                    className="flex items-center gap-2 text-[#D3E97A] text-sm border border-t-0 border-l-0 border-r-0 border-b-[#D3E97A] hover:text-white transition-colors duration-300"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <span className="font">LIVE DEMO</span>
+                    <ExternalLink size={16} />
+                  </a>
+                  <a
+                    href="https://github.com/skrillzofficial/Eventra"
+                    className="flex items-center gap-2 text-[#D3E97A] text-sm border border-t-0 border-l-0 border-r-0 border-b-[#D3E97A] hover:text-white transition-colors duration-300"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <span className="font">SEE ON GITHUB</span>
+                    <Github size={16} />
                   </a>
                 </div>
               </div>
--- src/components/WorkComponent.jsx
+++ src/components/WorkComponent.jsx
@@ -3,6 +3,7 @@ import { ExternalLink, Github } from "lucide-react";
 import linkshort from "../assets/Link shortner.png";
 import sentient from "../assets/sentient.png";
 import BetaHouse from "../assets/Beta H.png";
+import Eventra from "../assets/Eventra.png"
 
 const WorkComponent = () => {
   return (
@@ -28,8 +29,74 @@ const WorkComponent = () => {
             </p>
           </div>
         </div>
+        {/* Project 1 - Event Website */}
+        <div className="flex flex-col md:flex-row gap-10 items-start mb-32">
+          {/* Image container */}
+          <div className="w-full md:w-1/2 relative group">
+            <div className="bg-gray-300/10 rounded-lg p-6 flex flex-col h-full">
+              <div className="md:mb-5 bg-black w-40 text-center rounded-full mb-4">
+                <h3 className="text-lg font">Solo-Build Work</h3>
+              </div>
+              <div className="flex justify-center items-center p-4 h-80">
+                <img
+                  src={Eventra}
+                  alt="Landing page for shorter links"
+                  className="max-h-full max-w-full object-contain group-hover:scale-105 transition-transform duration-700"
+                />
+              </div>
+            </div>
+          </div>
 
-        {/* Project 1 - Link Shortener */}
+          {/* Content container*/}
+          <div className="w-full md:w-1/2 mt-10">
+            <h3 className="text-2xl font md:text-3xl font-bold text-white mb-4">
+              Your event, our entry
+            </h3>
+            <p className="font mb-6 leading-relaxed">
+              Eventry is the all-in-one event management platform that empowers you to seamlessly plan, organize, and execute any event, from large corporate conferences to deeply personal wedding celebrations. We provide the tools; you create the magic.
+            </p>
+
+            <div className="mb-4">
+              <p className="text-white">PROJECT INFO</p>
+            </div>
+
+            <div className="flex flex-col gap-2 border-t border-gray-800 pt-2">
+              <div className="flex font justify-between">
+                <span>Year</span>
+                <span>2025</span>
+              </div>
+              <div className="border-t font border-gray-800 pt-2">
+                <div className="flex justify-between">
+                  <span>Role</span>
+                  <span>Full-stack developer</span>
+                </div>
+              </div>
+            </div>
+
+            <div className="flex gap-4 mb-6 pt-4 border-t border-gray-800">
+              <a
+                href="https://eventra-yyo5.vercel.app//"
+                className="flex items-center gap-2 text-[#D3E97A] text-sm border border-t-0 border-l-0 border-r-0 border-b-[#D3E97A] hover:text-white transition-colors duration-300"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <span className="font">LIVE DEMO</span>
+                <ExternalLink size={16} />
+              </a>
+              <a
+                href="https://github.com/skrillzofficial/Eventra"
+                className="flex items-center gap-2 text-[#D3E97A] text-sm border border-t-0 border-l-0 border-r-0 border-b-[#D3E97A] hover:text-white transition-colors duration-300"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <span className="font">SEE ON GITHUB</span>
+                <Github size={16} />
+              </a>
+            </div>
+          </div>
+        </div>
+
+        {/* Project 2 - Link Shortener */}
         <div className="flex flex-col md:flex-row gap-10 items-start mb-32">
           {/* Image container */}
           <div className="w-full md:w-1/2 relative group">
@@ -99,7 +166,7 @@ const WorkComponent = () => {
           </div>
         </div>
 
-        {/* Project 2 - BetaHouse */}
+        {/* Project 3 - BetaHouse */}
         <div className="flex flex-col md:flex-row-reverse gap-10 items-start mb-32">
           {/* Image container*/}
           <div className="w-full md:w-1/2 relative group">
@@ -165,7 +232,7 @@ const WorkComponent = () => {
           </div>
         </div>
 
-        {/* Project 3 - E-commerce */}
+        {/* Project 4 - E-commerce */}
         <div className="flex flex-col md:flex-row gap-10 items-start mb-32">
           {/* Image container*/}
           <div className="w-full md:w-1/2 relative group">
@@ -235,7 +302,7 @@ const WorkComponent = () => {
           </div>
         </div>
 
-        {/* Project 4 - Sentient */}
+        {/* Project 5 - Sentient */}
         <div className="flex flex-col md:flex-row-reverse gap-10 items-start">
           {/* Image container*/}
           <div className="w-full md:w-1/2 relative group">
